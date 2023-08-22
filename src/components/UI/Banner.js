e8503b15f7344b5da011d53f235da5e820d89e3b
Shuvoghosh7 <shuvoghosh1610@gmail.com>
2023-08-23 00:25:59 +0600

    Add Banner Form

--- src/components/UI/Banner.js
+++ src/components/UI/Banner.js
@@ -5,6 +5,7 @@ import EagleVideo from "../../assets/images/banner/watermarked_preview.mp4";
 import styles from "@/styles/Banner.module.css";
 
 import Link from "next/link";
+import BannerForm from "./BannerForm";
 const Banner = () => {
   return (
     <div>
@@ -22,75 +23,9 @@ const Banner = () => {
         </div>
       </section>
 
-      <div className={styles.check_availability}>
-        <div class={styles.row}>
-          <div>
-            <h2>
-              {" "}
-              CHECK <br /> <span>AVAILABILITY</span>
-            </h2>
-          </div>
-          <div>
-            <form>
-              <div class={styles.availability_form}>
-                <div class={styles.awe_calendar_wrapper}>
-                  <label>Room Type</label> <br />
-                  <select class={styles.awe_calendar} name="room">
-                    <option selected="selected" disabled="disabled">
-                      Select a room
-                    </option>
-                    <option value="Single">Single room</option>
-                    <option value="Double">Double Room</option>
-                    <option value="Deluxe">Deluxe room</option>
-                  </select>
-                </div>
-                <div className={`${styles.awe_calendar_wrapper} ml-3`}>
-                  <label>Check-in</label> <br />
-                  <input
-                    type="date"
-                    name="arrive"
-                    class={styles.awe_calendar}
-                    placeholder="Arrival Date"
-                  />
-                </div>
-                <div className={`${styles.awe_calendar_wrapper} ml-3`}>
-                  <label>Check-out</label> <br />
-                  <input
-                    type="date"
-                    name="departure"
-                    class={styles.awe_calendar}
-                    placeholder="Arrival Date"
-                  />
-                </div>
-                <div className={`${styles.awe_calendar_wrapper} ml-3`}>
-                  <label>Adults</label> <br />
-                  <input
-                    type="number"
-                    name="adults"
-                    class={styles.awe_member}
-                    placeholder="Adults"
-                  />
-                </div>
-                <div className={`${styles.awe_calendar_wrapper} ml-3`}>
-                  <label>Children</label> <br />
-                  <input
-                    type="number"
-                    name="children"
-                    class={styles.awe_member}
-                    placeholder="Children"
-                  />
-                </div>
-
-                <div className={`${styles.awe_calendar_wrapper} ml-3`}>
-                  <button type="submit" class={styles.book_btn}>
-                    Book Now
-                  </button>
-                </div>
-              </div>
-            </form>
-          </div>
-        </div>
-      </div>
+      <section className={styles.banner_form}>
+        <BannerForm/>
+      </section>
     </div>
   );
 };
--- src/components/UI/FavoriteRooms.js
+++ src/components/UI/FavoriteRooms.js
@@ -2,56 +2,103 @@ import styles from "@/styles/FavoriteRooms.module.css";
 
 import Image from "next/image";
 import Link from "next/link";
-import { library } from '@fortawesome/fontawesome-svg-core';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faCheckCircle } from '@fortawesome/free-solid-svg-icons';
+import { library } from "@fortawesome/fontawesome-svg-core";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faCheckCircle } from "@fortawesome/free-solid-svg-icons";
 const FavoriteRooms = () => {
-    return (
-        <div>
-            <div className={styles.froom_title}>
-                {/* <h2>Guests Favorite Rooms</h2> */}
+  return (
+    <div>
+      <div className={styles.froom_title}>
+        <h2>Guests Favorite Rooms</h2>
+      </div>
+
+      <div className={styles.favorit_room_container}>
+        <div class={styles.room_thumb}>
+          <Image
+            src="https://i.ibb.co/q0QPSqc/image1.jpg"
+            width={400}
+            height={250}
+            alt="Picture of the author"
+            className={styles.gphoto}
+          />
+          <div class={`${styles.mask}`}>
+            <div class={`${styles.main}`}>
+              <h5>Double bedroom</h5>
+              <div class={styles.price}>
+                $ 99
+                <br /> <span>a night</span>
+              </div>
             </div>
-
-            <div className={styles.favorit_room_container}>
-                <div class={styles.room_thumb}>
-                    <Image
-                        src="https://i.ibb.co/q0QPSqc/image1.jpg"
-                        width={400}
-                        height={250}
-                        alt="Picture of the author"
-                        className={styles.gphoto}
-                    />
-                    <div class={`${styles.mask}`}>
-                        <div class={`${styles.main}`}>
-                            <h5>Double bedroom</h5>
-                            <div class={styles.price}>$ 99<br /> <span>a night</span></div>
-                        </div>
-                        <div class={`${styles.content} ${styles.hoverMask}`}>
-                            <p><span>A modern hotel room in Star Hotel</span> <br></br> Nunc tempor erat in magna pulvinar fermentum. Pellentesque scelerisque at leo nec vestibulum.
-                                malesuada metus.</p>
-                            <div className="flex justify-evenly items-center">
-                                <div>
-                                    <ul>
-                                        <li><span> <FontAwesomeIcon icon={faCheckCircle} /> </span> Incl. breakfast</li>
-                                    </ul>
-                                </div>
-                                <div>
-                                    <ul>
-                                        <li>Free Wi-Fi</li>
-                                    </ul>
-                                </div>
-                            </div>
-                            
-                            <Link href="/" class={styles.btn_block}>Read More</Link>
-                        </div>
-                    </div>
-
-
+            <div class={`${styles.content} ${styles.hoverMask}`}>
+              <p>
+                <span>A modern hotel room in Star Hotel</span> <br></br> Nunc
+                tempor erat in magna pulvinar fermentum. Pellentesque
+                scelerisque at leo nec vestibulum. malesuada metus.
+              </p>
+              <div className="flex justify-between items-center mx-5">
+                <div>
+                  <ul>
+                    <li>
+                      <span>
+                        {" "}
+                        <FontAwesomeIcon icon={faCheckCircle} />{" "}
+                      </span>{" "}
+                      Incl. breakfast
+                    </li>
+                    <li>
+                      <span>
+                        {" "}
+                        <FontAwesomeIcon icon={faCheckCircle} />{" "}
+                      </span>{" "}
+                      Private balcony
+                    </li>
+                    <li>
+                      <span>
+                        {" "}
+                        <FontAwesomeIcon icon={faCheckCircle} />{" "}
+                      </span>{" "}
+                      Sea view
+                    </li>
+                  </ul>
                 </div>
+                <div>
+                  <ul>
+                    <li>
+                      <span>
+                        {" "}
+                        <FontAwesomeIcon icon={faCheckCircle} />{" "}
+                      </span>
+                      Free Wi-Fi
+                    </li>
+                    <li>
+                      <span>
+                        {" "}
+                        <FontAwesomeIcon icon={faCheckCircle} />{" "}
+                      </span>{" "}
+                      Incl. breakfast
+                    </li>
+                    <li>
+                      <span>
+                        {" "}
+                        <FontAwesomeIcon icon={faCheckCircle} />{" "}
+                      </span>{" "}
+                      Bathroom
+                    </li>
+                  </ul>
+                </div>
+              </div>
+
+              <div className="mx-5">
+                <Link href="/" class={styles.btn_block}>
+                  Read More
+                </Link>
+              </div>
             </div>
-          
+          </div>
         </div>
-    )
+      </div>
+    </div>
+  );
 };
 
 export default FavoriteRooms;
