7e383765dec160c6e54d174019dc3c67f77931e2
Shuvoghosh7 <shuvogosh1610@gmail.com>
2023-10-19 14:31:35 +0600

    add content in contact page

--- src/pages/contact/index.js
+++ src/pages/contact/index.js
@@ -4,6 +4,11 @@ import Head from "next/head";
 import Image from "next/image";
 import himage from "../../assets/images/contact/contact-us-hero-illustration.svg";
 import RootLayout from "@/components/Layouts/RootLayout";
+import QC from "../../assets/images/contact/Our-Quick-Communication.png";
+import call from "../../assets/images/contact/Call.png";
+import Skype from "../../assets/images/contact/Skype-.png";
+import mail from "../../assets/images/contact/mail.png";
+import whatsapp from "../../assets/images/contact/whatsapp.png";
 const contact = () => {
     return (
         <div className="mt-40 mt-12 lg:mx-32 mx-5">
@@ -33,9 +38,9 @@ const contact = () => {
                 <div>
                     <div className={styles.forms}>
                         <div className={styles.reservation_vertical}>
-                           
+
                             <form >
-                                <div className={styles.form_group}>               
+                                <div className={styles.form_group}>
                                     <input name="text" type="text" className={styles.form_control} placeholder="Please Enter Your Name" />
                                 </div>
                                 <div className={`${styles.form_group} mt-5`}>
@@ -45,8 +50,8 @@ const contact = () => {
                                     <input name="number" type="text" className={styles.form_control} placeholder="Please Enter Your Number" />
                                 </div>
                                 <div className={`${styles.form_group} mt-5`}>
-    
-                                    <textarea name="message" className={styles.form_control_textarea} placeholder="Please Enter Your Massage"/>
+
+                                    <textarea name="message" className={styles.form_control_textarea} placeholder="Please Enter Your Massage" />
                                 </div>
 
                                 <div className={`${styles.form_group} mt-5`}>
@@ -60,6 +65,92 @@ const contact = () => {
 
                 </div>
             </div>
+
+            {/* Quick Communication */}
+            <div className={styles.quick_communication}>
+                <div className={styles.quick_communication_content}>
+                    <Image
+                        src={QC}
+                        width={52}
+                        height={45}
+                        alt="Picture of the author"
+                        className={styles.post_img}
+                    />
+                    <div className="ml-5">
+                        <h3>Our Quick Communication</h3>
+                        <h4><strong>24/7 support</strong> team is an always available helping hand, day or night!</h4>
+                    </div>
+                </div>
+
+
+                <div className={styles.communication_way}>
+                    <div className={styles.call}>
+                        <Image
+                            src={call}
+                            width={50}
+                            height={50}
+                            alt="Picture of the author"
+                            className={styles.post_img}
+                        />
+                        <div className={styles.blurb_texts}>
+                            <a href="tel:+13156364807">
+                                <h5>Tap/Click to Call</h5>
+                                <p>+1 315 636 4807</p>
+                            </a>
+                        </div>
+
+                    </div>
+                    <div className={styles.call}>
+                        <Image
+                            src={Skype}
+                            width={50}
+                            height={50}
+                            alt="Picture of the author"
+                            className={styles.post_img}
+                        />
+                        <div className={styles.blurb_texts}>
+                            <a href="skype:offshore.clipping?call">
+                                <h5>Tap/Click to Call</h5>
+                                <p>HoteleTONight</p>
+                            </a>
+                        </div>
+
+                    </div>
+                    <div className={styles.call}>
+                        <Image
+                            src={mail}
+                            width={50}
+                            height={50}
+                            alt="Picture of the author"
+                            className={styles.post_img}
+                        />
+                        <div className={styles.blurb_texts}>
+                            <a href="mailto:contact@hotatonight.com">
+                                <h5>Tap/Click to Email</h5>
+                                <p>contact@hotatonight.com</p>
+                            </a>
+                        </div>
+
+                    </div>
+                    <div className={styles.call}>
+                        <Image
+                            src={whatsapp}
+                            width={50}
+                            height={50}
+                            alt="Picture of the author"
+                            className={styles.post_img}
+                        />
+                        <div className={styles.blurb_texts}>
+                            <a href="https://wa.me/+13156364807">
+                                <h5>Tap/Click to Call</h5>
+                                <p>+8801772385111</p>
+                            </a>
+                        </div>
+
+                    </div>
+                </div>
+
+            </div>
         </div>
     )
 };
@@ -68,5 +159,4 @@ export default contact;
 
 contact.getLayout = function getLayout(page) {
     return <RootLayout>{page}</RootLayout>;
-  };
-  
+};
