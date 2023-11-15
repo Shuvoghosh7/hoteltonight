94ddb17bcfa754b88809291f4f646a6d01e3cd5b
Shuvoghosh7 <shuvogosh1610@gmail.com>
2023-11-15 18:01:22 +0600

    add comment reply

---
+++ src/components/UI/Comment/CommentReply.js
@@ -0,0 +1,16 @@
+import React from "react"
+import styles from "@/styles/LatestNews.module.css";
+const CommentReply = () => {
+    return (
+        <div className={styles.post_reply_container}>
+            <div className={styles.comment_reply_container}>
+                <h1>ADMIN</h1>
+                <p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat</p>
+            </div>
+
+
+        </div>
+    )
+};
+
+export default CommentReply;
--- src/components/UI/Comment/Commont.js
+++ src/components/UI/Comment/Commont.js
@@ -1,29 +1,53 @@
-import React from "react"
+import React, { useState } from "react"
 import styles from "@/styles/LatestNews.module.css";
 import blog1 from "../../../assets/images/LN/blog-post1.jpg";
 import Image from "next/image";
+import Popup from "../Popup/Popup";
+import CommentReply from "./CommentReply";
 const Comment = () => {
-    return (
-        <div className={styles.post_view_container}>
+    const [showPopup, setShowPopup] = useState(false);
 
-
-            <div className={styles.comment_avatar}>
-                <Image
-                    src={blog1}
-                    width={150}
-                    height={100}
-                    alt="Picture of the author"
-                    className={styles.comment_author_img}
-                />
-
-            </div>
-
-            <div className={styles.comment_info_container}>
-                <h1>JANE DOE</h1>
-                <p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat</p>
-            </div>
-
+    const handleLearnMoreClick = () => {
+        setShowPopup(true);
+    };
+    const handleClosePopup = () => {
+        setShowPopup(false);
+    };
+    const content = (
+        <div>
+            <p>
+                tempor erat in magna pulvinar fermentum. Pellentesque
+            </p>
         </div>
+    );
+    return (
+        <>
+            <div className={styles.post_view_container}>
+
+
+                <div className={styles.comment_avatar}>
+                    <Image
+                        src={blog1}
+                        width={150}
+                        height={100}
+                        alt="Picture of the author"
+                        className={styles.comment_author_img}
+                    />
+
+                </div>
+
+                <div className={styles.comment_info_container}>
+                    <h1>JANE DOE</h1>
+                    <p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat</p>
+                    <button onClick={handleLearnMoreClick}>reply</button>
+                </div>
+
+
+                {showPopup && <Popup content={content} onClose={handleClosePopup} />}
+            </div>
+            <CommentReply/>
+        </>
+
     )
 };
 
